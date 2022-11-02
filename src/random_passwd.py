0ced18e2517d1a6e732c1a79dba5c0834765a6e8
Carlos Sánchez recio <sanchezreciocarlos99@gmail.com>
2022-11-02 12:48:14 +0100

    New structure created - Modified previous file, same functionallity

---
+++ src/random_passwd.py
@@ -0,0 +1,36 @@
+import random
+
+# Define user custom errors
+# define Python user-defined exceptions
+
+
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+
+
+class not_valid_num(Error):
+    """Raised when the input number is not valid"""
+    pass
+
+
+def random_passwd(num):
+    try:
+        if num < 8 or num > 64:
+            raise not_valid_num
+
+        char = '!#$%&()*+,-./0123456789?@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz'
+        passwd = ''
+        for n in range(num):
+            passwd += char[random.randint(0, len(char)-1)]
+
+        return passwd
+    except not_valid_num:
+        print("The input value is not valid. Please introduce a number between 8 and 64")
+        # exit()
+    except ValueError:
+        print("Incorrect value introduced!")
+        # exit()
+    except:
+        print("Something went wrong")
+        # exit()
