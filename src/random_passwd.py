bf6354e5c0c0674d1e9ae719e6b4f5f9a896a440
Carlos Sánchez recio <sanchezreciocarlos99@gmail.com>
2022-11-02 16:17:39 +0100

    Added random pin function

--- src/random_passwd.py
+++ src/random_passwd.py
@@ -34,3 +34,25 @@ def random_passwd(num):
     except:
         print("Something went wrong")
         # exit()
+
+
+def random_pin(num):
+    try:
+        if num < 4 or num > 8:
+            raise not_valid_num
+
+        char = '0123456789'
+        pin = ''
+        for n in range(num):
+            pin += char[random.randint(0, len(char)-1)]
+
+        return pin
+    except not_valid_num:
+        print("The input value is not valid. Please introduce a number between 8 and 64")
+        # exit()
+    except ValueError:
+        print("Incorrect value introduced!")
+        # exit()
+    except:
+        print("Something went wrong")
+        # exit()
