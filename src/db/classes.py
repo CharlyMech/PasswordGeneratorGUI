ef8b2d372e3e73381484c8f0d94a33c21bf8ecde
Carlos Sánchez recio <sanchezreciocarlos99@gmail.com>
2022-11-13 19:56:47 +0100

    Classes: insert method working. Main: final GUI->FLET; working on how elements are displayed. For now, all 4 tabs created and ready to get filled with the content.

--- src/db/classes.py
+++ src/db/classes.py
@@ -1,9 +1,9 @@
 import sqlite3
 from os import getcwd
 
-'''
+"""
 # ?    Function that creates the DB
-'''
+"""
 
 
 def data_base():
@@ -15,94 +15,45 @@ def data_base():
         conn = sqlite3.connect(f"{pwd}/src/db/data_base.db")
         # Create the cursor to execute the table creation sentences
         cursor = connection.cursor()
-        cursor.execute("""
+        cursor.execute(
+            """
 			  create table if not exists passwords (
 				  passwd_id integer primary key autoincrement,
 				  passwd text not null,
 				  description text,
 				  creation_date text
 			  )
-		  """)
+		  """
+        )
 
-        cursor.execute("""
+        cursor.execute(
+            """
 			  create table if not exists pin (
 				  pin_id integer primary key autoincrement,
 				  pin integer not null,
 				  description text,
 				  creation_date text
 			  )
-		  """)
+		  """
+        )
         conn.commit()
 
     # If an sql error happens:
     except sqlite3.Error as e:
         print(e)
 
-
-# class Password():
-#     def __init__(self):
-#         super().__init__(__directory)
-#         try:
-#             self.__connection = sqlite3.connect(
-#                 f"{self.__directory}/src/db/data_base")
-#             self.__cursor = self.__connection.cursor()
-#         except sqlite3.Error as e:
-#             print(e)
-
-#     def passwd_insert(self, password, descr):
-#         data = [password, descr]
-#         try:
-#             sql = '''INSERT INTO passwords(passwd, description, creation_date) VALUES (?,?,(SELECT strftime('%d/%m/%Y', 'now')))'''
-#             sql_exec = self.__cursor.execute(sql, data)
-#             self.__connection.commit()
-#             self.__cursor.close()
-#         except sqlite3.Error as e:
-#             print(e)
-
-#     # def passwd_update(self, passwd_id):
-#     #     data = [password, descr]
-#     #     try:
-#     #         sql = '''INSERT INTO passwords(passwd, description, creation_date) VALUES (?,?,(SELECT strftime('%d/%m/%Y', 'now')))'''
-#     #         sql_exec = self.__cursor.execute(sql, data)
-#     #         self.__connection.commit()
-#     #         self.__cursor.close()
-#     #     except sqlite3.Error as e:
-#     #         print(e)
-
-#     def passwd_delete(self, passw_id):
-#         try:
-#             sql = '''DELETE FROM passwords WHERE passwd_id = ?'''
-#             sql_exec = self.__cursor.execute(sql, passw_id)
-#             self.__connection.commit()
-#         except sqlite3.Error as e:
-#             print(e)
-#         finally:
-#             self.__cursor.close()
-
-#     def passwd_select(self, passw_id):
-#         try:
-#             sql = '''SELECT * FROM passwords WHERE passwd_id = ?'''
-#             sql_exec = self.__cursor.execute(sql, passw_id)
-#             self.__connection.commit()
-#         except sqlite3.Error as e:
-#             print(e)
-#         finally:
-#             self.__cursor.close()
-
-#     def passwd_select_all(self):
-#         try:
-#             sql = '''SELECT * FROM passwords'''
-#             sql_exec = self.__cursor.execute(sql)
-#             self.__connection.commit()
-#             select = self.__cursor.fetchall()
-#             return select
-#         except sqlite3.Error as e:
-#             print(e)
-#         finally:
-#             self.__cursor.close()
+    return connection
 
 
-# obj = Password()
-# obj.passwd_insert('testeo1', '')
+class Password:
+    def __init__(self):
+        self.__conn = data_base()
 
-# # obj.passwd_select(3)
+    def insert_passwd(self, new_passwd, descr, date):
+        cur = self.__conn.cursor()
+        try:
+            sentence = f"INSERT INTO passwords('passwd','description','creation_date') VALUES (?,?,?)"
+            cur.execute(sentence, [new_passwd, descr, date])
+            self.__conn.commit()
+        except sqlite3.Error as e:
+            print(e)
--- src/main.py
+++ src/main.py
@@ -1,135 +1,52 @@
-import PySimpleGUI as sg
+import flet
+from flet import *
 from random_passwd import *
-from db.classes import data_base
+from db.classes import *
 from os import getcwd
+from datetime import date
 
 
-sg.theme('DarkTanBlue')
+def main(page: Page):
 
-# Left pannel buttons
-left_pannel = [[sg.Button('New Password', size=(20, 3), font='36', key="rand_passwd")], [sg.T("")],
-               [sg.Button('New Pin', size=(20, 3), font='36', key="rand_pin")], [
-    sg.T("")],
-    [sg.Button('Input Password', size=(20, 3),
-               font='36', key="new_passwd")], [sg.T("")],
-    [sg.Button('Saved', size=(20, 3), font='36', key="stored")]]
+    page.window_width = 350
+    page.window_height = 200
+    page.window_resizable = False
 
+    t = Tabs(
+        selected_index=0,
+        animation_duration=300,
+        tabs=[
+            Tab(
+                text="Random Password",
+                content=Container(
+                    content=Text("Random password generation"),
+                    alignment=alignment.center,
+                ),
+            ),
+            Tab(
+                text="Random Pin",
+                content=Container(
+                    content=Text("Random pin generation"), alignment=alignment.center
+                ),
+            ),
+            Tab(
+                text="Insert Password",
+                content=Container(
+                    content=Text("Custom password insert"), alignment=alignment.center
+                ),
+            ),
+            Tab(
+                text="Stored",
+                content=Container(
+                    content=Text("All passwords and pin stored"),
+                    alignment=alignment.center,
+                ),
+            ),
+        ],
+        expand=1,
+    )
 
-# Define main contents
-main = [
-    # Content at execution
-    [sg.pin(sg.T("Welcome to Random Password Generator", size=(
-        50, 1), justification='c', key="main_title", font='40', visible=True))],
-    [sg.pin(sg.T("", size=(50, 5),
-            key="hidden_line", visible=True))],
-    [sg.Image(f"{getcwd()}/src/img/lock_00_128.png",  size=(
-        150, 150), key="lock_display", visible=True)],
-
-    # Ramdom Password generator
-    [sg.pin(sg.Text("Generate New random password", size=(
-        50, 1),  justification='c', key="gen_rand_passwd_TITLE", font='24', visible=False))],
-    [sg.pin(sg.Button("Generate", size=(10, 1),
-                      key="gen_rand_passwd_BUTTON",  font='18', visible=False))],
-    [sg.pin(sg.InputText("Random Password", text_color='gray', size=(25, 1), use_readonly_for_disable=True, disabled=False, justification='c',
-                         key="gen_rand_passwd_PASSWD", font='24', visible=False)), sg.pin(sg.Button("Copy", size=(4, 1),
-                                                                                                    key="gen_rand_passwd_COPY",  font='18', visible=False))],
-    [sg.pin(sg.Button("Save", size=(10, 1),
-                      key="gen_rand_passwd_SAVE", font='18', visible=False))],
-
-    # Random PIN generator
-    [sg.pin(sg.Text("New Random pin Selected", size=(80, 1), justification='c',
-                    key="rand_pin_display", visible=False))],
-
-    # Password manual insertion
-    [sg.pin(sg.Text("Insert a Passwd Selected", size=(80, 1), justification='c',
-                    key="insert_passwd_display", visible=False))],
-
-    # Stored passwords and pin
-    [sg.pin(sg.Text("Passwd and PIN DB display", size=(80, 1), justification='c',
-                    key="stored_db_display", visible=False))],
-]
-
-# All layout and window generation
-layout = [[sg.Column(left_pannel, element_justification='c'),
-           sg.VSep(), sg.Column(main, element_justification='c')]]
-
-window = sg.Window('Random Password Generator', layout)
+    page.add(t)
 
 
-# MAIN #
-if __name__ == "__main__":
-    data_base()
-    # Program loop
-    while True:
-        event, values = window.read()
-
-        # Style for generated passwd
-        #! Commented -> When close window error appears idk why
-        # window['gen_rand_passwd_PASSWD'].Widget.config(
-        #     readonlybackground=sg.theme_background_color())
-        # window['gen_rand_passwd_PASSWD'].Widget.config(borderwidth=0)
-
-        # Random password
-        if event == 'rand_passwd':
-            window['lock_display'].Update(visible=False)
-            window['main_title'].Update(visible=False)
-            window['hidden_line'].Update(visible=False)
-            window['rand_pin_display'].Update(visible=False)
-            window['stored_db_display'].Update(visible=False)
-            window['insert_passwd_display'].Update(visible=False)
-
-            window['gen_rand_passwd_TITLE'].Update(visible=True)
-            window['gen_rand_passwd_BUTTON'].Update(visible=True)
-            # window['gen_rand_passwd_PASSWD'].Update(visible=True)
-
-        elif event == 'gen_rand_passwd_BUTTON':
-            window['lock_display'].Update(visible=False)
-            window['main_title'].Update(visible=False)
-            window['hidden_line'].Update(visible=False)
-            window['rand_pin_display'].Update(visible=False)
-            window['stored_db_display'].Update(visible=False)
-            window['insert_passwd_display'].Update(visible=False)
-
-            window['gen_rand_passwd_PASSWD'].Update(visible=True)
-            window['gen_rand_passwd_PASSWD'].Update(disabled=True)
-            window['gen_rand_passwd_PASSWD'].Update(visible=True)
-            window['gen_rand_passwd_PASSWD'].Update(text_color='black')
-            window['gen_rand_passwd_PASSWD'].Update(random_passwd(12))
-            window['gen_rand_passwd_COPY'].Update(visible=True)
-            window['gen_rand_passwd_SAVE'].Update(visible=True)
-
-        # Random pin
-        if event == 'rand_pin':
-            window['lock_display'].Update(visible=False)
-            window['main_title'].Update(visible=False)
-            window['hidden_line'].Update(visible=False)
-            window['rand_passwd_display'].Update(visible=False)
-            window['stored_db_display'].Update(visible=False)
-            window['insert_passwd_display'].Update(visible=False)
-            window['rand_pin_display'].Update(visible=True)
-
-        # New password
-        if event == 'new_passwd':
-            window['lock_display'].Update(visible=False)
-            window['main_title'].Update(visible=False)
-            window['hidden_line'].Update(visible=False)
-            window['rand_pin_display'].Update(visible=False)
-            window['stored_db_display'].Update(visible=False)
-            window['rand_passwd_display'].Update(visible=False)
-            window['insert_passwd_display'].Update(visible=True)
-
-        # Show stored passwords and pin
-        if event == 'stored':
-            window['lock_display'].Update(visible=False)
-            window['main_title'].Update(visible=False)
-            window['hidden_line'].Update(visible=False)
-            window['rand_pin_display'].Update(visible=False)
-            window['insert_passwd_display'].Update(visible=False)
-            window['rand_passwd_display'].Update(visible=False)
-            window['stored_db_display'].Update(visible=True)
-
-        # Exit loop and program
-        if event == sg.WIN_CLOSED:
-            break
-
-    window.close()
+flet.app(target=main)
